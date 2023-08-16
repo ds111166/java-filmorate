333acf7274ac7ac58e9bcafcdb07568684fe0b1d
ds <d.a.shevelev@yandex.ru>
2023-08-16 14:14:49 +0300

    add methods in FilmStorage, UserStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,4 +1,10 @@
 package ru.yandex.practicum.filmorate.storage;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.util.List;
 
 public interface FilmStorage {
+    List<Film> getFilms();
+    Film crateFilm(Film newFilm);
+    Film updateFilm(Film updatedFilm);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,4 +1,11 @@
 package ru.yandex.practicum.filmorate.storage;
 
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.List;
+
 public interface UserStorage {
+    List<User> getUsers();
+    User crateUser(User newUser);
+    User updateUser(User updatedUser);
 }
